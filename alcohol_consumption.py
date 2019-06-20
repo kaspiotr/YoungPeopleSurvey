676ec512a3a82f01642ea604d6feae1772944264
meehaw1337-laptop <15misiek12@wp.pl>
2019-06-20 23:06:45 +0200

    weird stuff fixed xD

--- alcohol_consumption.py
+++ alcohol_consumption.py
@@ -15,11 +15,7 @@ def alcohol_consumption():
         total_by_age = dict()
         percentage_of_heavy_drinkers = []
 
-<<<<<<< HEAD
-        for i in range (15,31):
-=======
         for i in range(15, 31):
->>>>>>> 5809d9d00f0bab3f4fac2c44a55a88dfa97b99e0
             heavy_drinkers_by_age[i] = 0
             total_by_age[i] = 0
 
@@ -44,65 +40,36 @@ def alcohol_consumption():
             except ValueError:
                 pass
 
-<<<<<<< HEAD
-        for i in range (15,31):
-            percentage_of_heavy_drinkers.append(( heavy_drinkers_by_age[i] / total_by_age[i] ) * 100)
-=======
         for i in range(15, 31):
             percentage_of_heavy_drinkers.append((heavy_drinkers_by_age[i] / total_by_age[i]) * 100)
->>>>>>> 5809d9d00f0bab3f4fac2c44a55a88dfa97b99e0
 
         plt.subplot(231)
         plt.xlabel("Age")
         plt.title("Non drinkers")
         plt.ylim(0, 150)
-<<<<<<< HEAD
-        plt.hist(non_drinkers, 40, range=(15,30))
-=======
         plt.hist(non_drinkers, 40, range=(15, 30))
->>>>>>> 5809d9d00f0bab3f4fac2c44a55a88dfa97b99e0
 
         plt.subplot(232)
         plt.title("Social drinkers")
         plt.ylim(0, 150)
-<<<<<<< HEAD
-        plt.hist(social_drinkers, 40, range=(15,30))
-=======
         plt.hist(social_drinkers, 40, range=(15, 30))
->>>>>>> 5809d9d00f0bab3f4fac2c44a55a88dfa97b99e0
 
         plt.subplot(233)
         plt.xlabel("Age")
         plt.title("Heavy drinkers")
         plt.ylim(0, 150)
-<<<<<<< HEAD
-        plt.hist(heavy_drinkers, 40, range=(15,30))
-=======
         plt.hist(heavy_drinkers, 40, range=(15, 30))
->>>>>>> 5809d9d00f0bab3f4fac2c44a55a88dfa97b99e0
 
         plt.subplot(235)
         plt.xlabel("Age")
         plt.title("Percentage of heavy drinkers")
-<<<<<<< HEAD
-        ages = range(15,31)
-=======
         ages = range(15, 31)
->>>>>>> 5809d9d00f0bab3f4fac2c44a55a88dfa97b99e0
         plt.bar(ages, percentage_of_heavy_drinkers)
 
         plt.suptitle("Alcohol consumption by age and amount")
-
-        print(percentage_of_heavy_drinkers)
-<<<<<<< HEAD
-        plt.show()
-
         
-
-=======
         plt.rcParams['figure.figsize'] = [30, 15]
         plt.show()
 
->>>>>>> 5809d9d00f0bab3f4fac2c44a55a88dfa97b99e0
 
 alcohol_consumption()
